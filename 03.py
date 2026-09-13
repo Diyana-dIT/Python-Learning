089e6db388cff6f659ae1a447e79b0b71c1f3fe0
Diyana <diyanadargahiiiii@gmail.com>
2026-09-13 10:25:00 +0330

    session-06

---
+++ 03.py
@@ -0,0 +1,19 @@
+{
+'p': 1,
+'r': 2,
+'o': 1,
+'g': 2,
+'a': 1,
+'m': 2,
+'i': 1,
+'n': 1
+}
+text=input("Enter:")
+b={}
+for i in text:
+    if i.isalpha():
+        if i in b:
+            b[i] += 1
+        else:
+            b[i] = 1
+print(b)
