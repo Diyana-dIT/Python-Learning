a351f9a01498e2b7ceab431cdda90fb55a40a0fb
Diyana <diyanadargahiiiii@gmail.com>
2026-09-13 14:49:16 +0330

    Add files via upload

---
+++ question07.py
@@ -0,0 +1,9 @@
+orders=[("Ali","Laptop"),("Sara","Phone"),("Ali","Phone"),("Reza","Laptop"),("Sara","Laptop"),("Ali","Tablet"),("Reza","Phone")]
+
+result={}
+
+for customer,product in orders:
+    result[customer]=result.get(customer,[])
+    result[customer].append(product)
+
+print(result)
