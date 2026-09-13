1766f304487b6dd7e0c37b40bc1158e6cd484c56
Diyana <diyanadargahiiiii@gmail.com>
2026-09-13 14:50:31 +0330

    Update question07.py

--- question07.py
+++ question07.py
@@ -1,9 +1,7 @@
 orders=[("Ali","Laptop"),("Sara","Phone"),("Ali","Phone"),("Reza","Laptop"),("Sara","Laptop"),("Ali","Tablet"),("Reza","Phone")]
 
 result={}
-
 for customer,product in orders:
     result[customer]=result.get(customer,[])
     result[customer].append(product)
-
 print(result)
